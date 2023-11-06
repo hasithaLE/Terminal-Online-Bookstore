46642a2c7d7b5b092680e22504496d571ffe70a8
Hasitha Edirisinghe <hasithale@gmail.com>
2023-11-07 00:54:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Online_Bookstore
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("...Welcome to Online Bookstore...");
+			Console.WriteLine("Press 1 for Log In\nPress 2 for Register\nPress 3 for Skip and Go to Store");
+			string inputOne = Console.ReadLine();
+			if (inputOne == "1")
+			{
+				Console.WriteLine($"Enter Username and Password");
+			}
+			else if (inputOne == "2")
+			{
+				Console.WriteLine($"Enter Your Fullname, Unique Username and Strong Password");
+			}
+			else if (inputOne == "3") 
+			{
+				Console.WriteLine($"Welcome to the Store");
+			}
+			else
+			{
+				Console.WriteLine($"Default");
+			}
+
+        }
+	}
+}
